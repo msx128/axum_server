6df9c970d3316e6d1e22cbd67c1fddc921944174
msx128 <msx128@proton.me>
2026-06-29 21:09:34 +0600

    NOW we getting somewhere

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,20 @@
-use axum::{Extension, http::StatusCode, response::ErrorResponse, routing::post};
+use axum::{
+    Extension, Json,
+    http::StatusCode,
+    response::Redirect,
+    routing::{get, post},
+};
+use base64::{Engine, engine::general_purpose::URL_SAFE_NO_PAD};
 use dotenvy::dotenv;
 use rand::Rng;
 use serde::{Deserialize, Serialize};
 use sqlx::{
     FromRow,
     postgres::{PgConnectOptions, PgPool, PgPoolOptions},
-    types::Json,
 };
 use std::env;
 use std::{str::FromStr, sync::Arc};
+use tower_cookies::{Cookie, CookieManagerLayer, Cookies};
 use tower_http::services::{ServeDir, ServeFile};
 
 struct Database {
@@ -23,7 +29,7 @@ impl Database {
 
         let pool = PgPoolOptions::new().connect_with(db_opt).await?;
 
-        sqlx::query(
+        sqlx::query!(
             "CREATE TABLE IF NOT EXISTS users (
             id SERIAL PRIMARY KEY,
             nick TEXT NOT NULL UNIQUE,
@@ -33,17 +39,28 @@ impl Database {
         .execute(&pool)
         .await?;
 
+        sqlx::query!(
+            "CREATE TABLE IF NOT EXISTS sessions (
+                id SERIAL PRIMARY KEY,
+                user_id INTEGER NOT NULL REFERENCES users(id),
+                token TEXT NOT NULL UNIQUE,
+                created_at TIMESTAMP NOT NULL DEFAULT NOW()
+            );"
+        )
+        .execute(&pool)
+        .await?;
+
         Ok(Self { pool })
     }
 }
 
-#[derive(FromRow, Serialize, Deserialize)]
+#[derive(Serialize, Deserialize)]
 struct CreateUser {
     nick: String,
     pswd: String,
 }
 
-#[derive(FromRow, Serialize, Deserialize)]
+#[derive(Serialize, Deserialize)]
 struct UserResponse {
     id: i32,
     token: String,
@@ -71,7 +88,7 @@ async fn create_user(db: &Database, user: CreateUser) -> Result<i32, sqlx::Error
     Ok(created.id)
 }
 
-async fn insert_id_token(db: &Database, user: UserResponse) -> Result<(), sqlx::Error> {
+async fn insert_id_token(db: &Database, user: &UserResponse) -> Result<(), sqlx::Error> {
     let _ = sqlx::query!(
         r#"
             INSERT INTO sessions (user_id, token)
@@ -86,10 +103,13 @@ async fn insert_id_token(db: &Database, user: UserResponse) -> Result<(), sqlx::
     Ok(())
 }
 
+#[axum::debug_handler]
 async fn singin_handler(
+    cookies: Cookies,
     Extension(db): Extension<Arc<Database>>,
     Json(user): Json<CreateUser>,
-) -> Result<(StatusCode, Json<UserResponse>), StatusCode> {
+) -> Result<Json<UserResponse>, StatusCode> {
+    println!("{} {}", &user.nick, &user.pswd);
     let id = create_user(&db, user).await.map_err(|e| {
         if let sqlx::Error::Database(db_e) = &e {
             if db_e.code().as_deref() == Some("23505") {
@@ -102,13 +122,53 @@ async fn singin_handler(
 
     let push = UserResponse { id, token };
 
-    Ok((ST))
+    insert_id_token(&db, &push).await.map_err(|_| {
+        return StatusCode::INTERNAL_SERVER_ERROR;
+    })?;
+
+    let new_cookie: Cookie = Cookie::build(("session_token", push.token.clone()))
+        .path("/")
+        .http_only(true)
+        .build();
+
+    cookies.add(new_cookie);
+
+    Ok(Json(push))
 }
 
 fn make_rand_token() -> String {
     let mut buf = [0u8; 32];
     rand::rng().fill_bytes(&mut buf);
-    String::from_utf8(buf.to_vec()).unwrap()
+
+    let token = URL_SAFE_NO_PAD.encode(buf);
+
+    token
+}
+
+async fn button_page_handler(
+    cookies: Cookies,
+    Extension(db): Extension<Arc<Database>>,
+) -> Result<axum::response::Html<String>, Redirect> {
+    let cookie = cookies.get("session_token").ok_or(Redirect::to("/login"))?;
+
+    let token = cookie.value();
+
+    let _session = sqlx::query!(
+        "
+            SELECT user_id FROM sessions WHERE token = $1 AND created_at < NOW()
+        ",
+        &token
+    )
+    .fetch_one(&db.pool)
+    .await
+    .map_err(|_| Redirect::to("/login"))?; //don't need session, at least now
+
+    println!("logged via {}", token);
+
+    tokio::fs::read_to_string("./static/button/index.html")
+        .await
+        .map(axum::response::Html)
+        .map_err(|_| Redirect::to("/login")) //may god forbid me for this sin
 }
 
 #[tokio::main]
@@ -116,11 +176,13 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let state = Arc::new(Database::new().await?);
 
     let app = axum::Router::new()
-        // .route("/api/singin", post(singin_handler))
+        .route("/api/singin", post(singin_handler))
+        .route("/button", get(button_page_handler))
         .fallback_service(
             ServeDir::new("static").not_found_service(ServeFile::new("static/404.html")),
         )
-        .layer(Extension(state));
+        .layer(Extension(state))
+        .layer(CookieManagerLayer::new());
 
     let listener = tokio::net::TcpListener::bind("localhost:3000").await?;
 
--- static/js/login.js
+++ static/js/login.js
@@ -1,14 +1,32 @@
-const response = await fetch("api/auth", {
-  method: "POST",
-  headers: {
-    "Content-Type": "application/json"
-  },
-  body: JSON.stringify({nick, pswd})
-});
+const form = document.querySelector(".login-form");
 
-if (response.ok) {
-  window.location.href = "/";
-} else {
-  const e = await response.text();
-  console.log(e);
-}
+form.addEventListener("submit", async (e) => {
+  e.preventDefault();
+
+  const nick = document.getElementById("username").value;
+  const pswd = document.getElementById("password").value;
+
+  try {
+    const response = await fetch ("/api/singin", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify({nick, pswd})
+    });
+
+    console.log("Response status:", response.status);
+    console.log("Response OK:", response.ok);
+    
+    const data = await response.json();
+    console.log("Server response:", data);
+
+    if (response.ok) {
+      window.location.href = "/";
+    } else {
+      console.log("Error:", response.status, response.statusText);
+    }
+  } catch (error) {
+    console.error("fetch error:", error);
+  }
+});
